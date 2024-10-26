f63bd2740a1050e993c0b2252da834906bca802e
notPhani <phaniexplains14@gmail.com>
2024-10-26 09:26:06 +0530

    Update Data_collection.py
    Adding changes like:
     .coloring the command panel
     .And downloading the spectrum

--- Data_collection.py
+++ Data_collection.py
@@ -1,5 +1,5 @@
 # Importing the base modules
-import pandas as pd
+import pandas as pd #Used for reading the data from CSV
 import numpy as np
 import os
 import shutil
@@ -16,12 +16,11 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.firefox.options import Options
-
+#Folder monitoring 
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
 # Intitializing the options to an empty function to prevent errors
 options = Options()
-
-# Initializing the chrome driver
-
 
 # Enter the SDSS data collection webpage
 class Id_collection:
@@ -62,16 +61,38 @@ class Unpack_move:
         os.makedirs(collection_unit,exist_ok=True)
         shutil.move(file_name,os.path.join(collection_unit),os.path.basename(file_name))
 
+# Initializing monitoring systems
+class NewFileHandler(FileSystemEventHandler):
+    def on_created(self, event):
+        if not event.is_directory:
+            if os.path.basename(event.src_path).endswith(".fits.bz2"):
+                print(f"{event.src_path} is {Color.GREEN}downloaded")
+
+def monitor_folder(folder_path):
+    event_handler = NewFileHandler()
+    observer = Observer()
+    observer.schedule(event_handler, folder_path, recursive=False)
+    observer.start()
+    print(f"{Color.GREEN}Started monitoring folder: {Color.CYAN}{folder_path}")
+    try:
+        while True:
+            time.sleep(1)  
+    except KeyboardInterrupt:
+        pass
+        observer.stop()  # Stop the observer when interrupted (Ctrl+C)
+        #print("Stopped monitoring.")
+    observer.join()
+    
 
     
 # Intitialize the Id collection class
-Id_collection = Id_collection(10,"./optical_search_412544.csv")
+Id_collection = Id_collection(100,"./optical_search_412544.csv")
 spaceIds = Id_collection.get_Ids()
 #print(spaceIds)
 url_collection = Id_collection.url_const_im(spaceIds)
-print(url_collection)
+#print(url_collection)
 url_collection_spectrum = Id_collection.url_const_sp(spaceIds)
-print(url_collection_spectrum)
+#print(url_collection_spectrum)
 
 # Now we have the urls for all n_points number of galxies in a list
 # Next part is to access the url and download the filters and spectrum
@@ -93,29 +114,46 @@ def navigate_to_fits(url):
 
     # Download all filters in corrected frames
     filters = ["u","g","r","i","z"]
+    folder_path = "C:/Users/HP/Downloads"
+    event_handler = NewFileHandler()
+    observer = Observer()
+    observer.schedule(event_handler, folder_path, recursive=False)
+    observer.start()
+    print(f"{Color.GREEN}Started monitoring folder: {Color.CYAN}{folder_path}")
 
     # Loop for all the filters and download the first link.
     for filter in filters:
         wait_time = WebDriverWait(driver,10)
         download_link = wait_time.until(EC.presence_of_element_located((By.LINK_TEXT,filter)))
         time.sleep(3)
-        download_link.click()
+        download_link.click()     
     time.sleep(4)
     driver.quit()
+    observer.stop()
+    print(f"Filters downloaded {Color.GREEN}Successfully")
+    observer.join()
 
 def navigate_to_spectrum(url):
     driver = webdriver.Firefox()
     driver.get(url)
     wait = WebDriverWait(driver,15)
+    folder_path = "C:/Users/HP/Downloads"
+    event_handler = NewFileHandler()
+    observer = Observer()
+    observer.schedule(event_handler, folder_path, recursive=False)
+    observer.start()
+    print(f"{Color.GREEN}Started monitoring folder: {Color.CYAN}{folder_path}")
 
     spectrum_link = wait.until(EC.element_to_be_clickable((By.PARTIAL_LINK_TEXT,"Download")))
     spectrum_link.click()
     time.sleep(2)
     driver.quit()
-   
+    observer.stop()
+    print(f"Spectrum downloaded {Color.GREEN}Successfully")
+    observer.join()  
 #running a loop
 
-for i in range(3):
+for i in range(1):
     navigate_to_fits(url_collection[i])
     navigate_to_spectrum(url_collection_spectrum[i])
 
@@ -133,6 +171,7 @@ for i in range(len(file_list)):
     unpacked_file = Unpack_move.unpack_fits(ftbu)
     #moving to folder
     Unpack_move.move_rename_images(unpacked_file,folder_id)
+    print(f"{Color.GREEN}Moved {unpacked_file} into Galaxy{folder_id}/images ")
 
 #Formating the spectrum into the respective files
 
@@ -142,3 +181,4 @@ spec_file_list.sort(key = os.path.getmtime,reverse=True)
 for i in range(len(spec_file_list)):
     folder_id = i+1
     Unpack_move.move_rename_spectrum(spec_file_list[i],folder_id)
+    print(f"{Color.GREEN}Moved {spec_file_list[i]} into Galaxy{folder_id}/spectrum ")
